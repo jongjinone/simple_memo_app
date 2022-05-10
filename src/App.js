c08e794d57f2d9c3f3e4387fe4f3227d21a8c53e
jongjinone <wis06003@naver.com>
2022-05-10 22:21:33 +0900

    memo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [buttonState, setButtonState] = useState("green")
+  const [memoList, setMemoList] = useState([])
+  const [currentTab, setCurrentTab] = useState('write')
+
+  const onTitleChange = (evt) => {
+    console.log(evt.target.value)
+    setTitle(evt.target.value)
+  }
+
+  const onContentChange = (evt) => {
+    console.log(evt.target.value)
+    setContent(evt.target.value)
+  }
+
+
+  const SaveData = () => {
+    if (buttonState == "green") {
+      setButtonState("yellow")
+    } else {
+      setButtonState("green")
+    }
+
+    const cp = [...memoList]
+    cp.push({
+      title: title,
+      content: content,
+      createdAt: "2020"
+    })
+
+    setMemoList(cp)
+    alert("메모가 저장되었습니다.")
+    setCurrentTab('list')
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="ChoiceButton">
+        <div className={`TabBtn ${currentTab === "write" ? "active" : ""}`} onClick={(evt) => setCurrentTab('write')}>write</div>
+        <div className={`TabBtn ${currentTab === "list" ? "active" : ""}`} onClick={(evt) => setCurrentTab('list')}> list </div>
+      </div>
+
+      {currentTab === "write" && <div>
+        <input className="input" placeholder="title" onChange={(evt) => onTitleChange(evt)} />
+        <input className="input" placeholder="content" onChange={(evt) => onContentChange(evt)} />
+        <button className="button" onClick={(evt) => SaveData()}>save</button>
+      </div>}
+
+      {currentTab ==="list" &&
+      <div>
+      {memoList.map((obj) => {
+        return (
+          <div>
+            <div >{obj.title}</div>
+            <div>{obj.content}</div>
+          </div>)
+      }
+      )}
+    </div>
+      }
+
     </div>
   );
 }
