94c8fba08631d2b097a1ddb8439a4b6fdb4ee203
jongjinone <78363777+jongjinone@users.noreply.github.com>
2022-05-29 16:15:53 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import Memo from './pages/Memo';
 import MemoModify from './pages/MemoModify';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={< Memo />}></Route>
@@ -16,10 +13,4 @@ root.render(
         <Route path="/modify/:id" element={< MemoModify />}></Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
